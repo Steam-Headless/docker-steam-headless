cfd7bbda0ec9bdb9219a2eee6913a20b59492334
Josh.5 <jsunnex@gmail.com>
2022-10-02 11:27:41 +0000

    Improvements to pulseaudio
    - Add a DEBUGGING flag
    - Make the location of the unix socket configurable
    
    While I was here I moved all the priority configs in supervisord to the top. I like it better there

---
+++ overlay/usr/bin/start-pulseaudio.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+###
+# File: start-desktop.sh
+# Project: bin
+# File Created: Thursday, 1st January 1970 12:00:00 pm
+# Author: Josh.5 (jsunnex@gmail.com)
+# -----
+# Last Modified: Sunday, 2nd October 2022 22:58:17 pm
+# Modified By: Josh.5 (jsunnex@gmail.com)
+###
+
+# CATCH TERM SIGNAL:
+_term() {
+    kill -TERM "$pulseaudio_pid" 2>/dev/null
+}
+trap _term SIGTERM
+
+
+# EXECUTE PROCESS:
+echo "PULSEAUDIO: Starting pulseaudio service"
+sleep 5
+#/usr/bin/pulseaudio --disallow-module-loading --disallow-exit --exit-idle-time=-1 &
+/usr/bin/pulseaudio --exit-idle-time=-1 &
+pulseaudio_pid=$!
+
+
+# WAIT FOR CHILD PROCESS:
+wait "$pulseaudio_pid"
