71da6e4e8994ebb8d1affbcab67d3751662c9ddc
Josh.5 <jsunnex@gmail.com>
2022-10-02 11:37:43 +0000

    Fix desktop and udev launch scripts
    These services were not responding to a SIGTERM
    We need to catch the SIGTERM from the parent script and kill the child process

--- overlay/usr/bin/start-udev.sh
+++ overlay/usr/bin/start-udev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ###
-# File: start-xorg.sh
+# File: start-udev.sh
 # Project: bin
 # File Created: Tuesday, 12th January 2022 8:46:47 am
 # Author: Josh.5 (jsunnex@gmail.com)
@@ -9,15 +9,33 @@
 # Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 
+# CATCH TERM SIGNAL:
+_term() {
+    kill -TERM "$monitor_pid" 2>/dev/null
+    kill -TERM "$trigger_pid" 2>/dev/null
+    kill -TERM "$udevd_pid" 2>/dev/null
+}
+trap _term SIGTERM
+
+
+# EXECUTE PROCESS:
 # Start udev
 # Source: https://github.com/balena-io-playground/balena-base-images/
-if command -v udevd &> /dev/null; then
-    unshare --net udevd --daemon &> /dev/null
+if command -v udevd &>/dev/null; then
+    unshare --net udevd --daemon &>/dev/null
+    udevd_pid=$!
 else
-    unshare --net /lib/systemd/systemd-udevd --daemon &> /dev/null
+    unshare --net /lib/systemd/systemd-udevd --daemon &>/dev/null
+    udevd_pid=$!
 fi
-udevadm trigger &> /dev/null
+udevadm trigger &>/dev/null
+trigger_pid=$!
+# Monitor kernel uevents
+udevadm monitor &
+monitor_pid=$!
 
 
-# Monitry kernel uevents
-udevadm monitor
+# WAIT FOR CHILD PROCESS:
+wait "$monitor_pid"
+wait "$trigger_pid"
+wait "$udevd_pid"
