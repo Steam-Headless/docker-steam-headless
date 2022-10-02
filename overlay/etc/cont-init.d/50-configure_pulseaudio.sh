7f1baee34c880a087a9df41353a6f93b1271db92
Josh.5 <jsunnex@gmail.com>
2022-10-02 08:37:11 +0000

    Prevent pulseaudio from respawning
    https://unix.stackexchange.com/questions/204522/how-does-pulseaudio-start

--- overlay/etc/cont-init.d/50-configure_pulseaudio.sh
+++ overlay/etc/cont-init.d/50-configure_pulseaudio.sh
@@ -11,9 +11,13 @@ else
     echo "Configure pulseaudio to pipe audio to a socket"
     mkdir -p /tmp/pulse
     chmod -R a+rw /tmp/pulse
+    # Configure the palse audio socket
     sed -i 's|^; default-server.*$|default-server = unix:/tmp/pulse/pulse-socket|' /etc/pulse/client.conf
     sed -i 's|^load-module module-native-protocol-unix.*$|load-module module-native-protocol-unix socket=/tmp/pulse/pulse-socket auth-anonymous=1|' \
         /etc/pulse/default.pa
+    # Disable pulseaudio from respawning (https://unix.stackexchange.com/questions/204522/how-does-pulseaudio-start)
+    sed -i 's|^; autospawn.*$|autospawn = no|' /etc/pulse/client.conf
+    sed -i 's|^; daemon-binary.*$|daemon-binary = /bin/true |' /etc/pulse/client.conf
 fi
 chown -R ${USER} /etc/pulse
 
