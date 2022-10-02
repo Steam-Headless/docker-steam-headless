ddae528e623e8541918472623b5c8a146b9bc6e0
Josh.5 <jsunnex@gmail.com>
2022-10-02 06:47:06 +0000

    Set the pulse audio socket to a more standard path
    This path is easily shared with other containers

--- overlay/etc/cont-init.d/50-configure_pulseaudio.sh
+++ overlay/etc/cont-init.d/50-configure_pulseaudio.sh
@@ -9,8 +9,10 @@ if [ "${MODE}" == "s" ] | [ "${MODE}" == "secondary" ]; then
     sed -i 's|^; flat-volumes.*$|flat-volumes = yes|' /etc/pulse/daemon.conf
 else
     echo "Configure pulseaudio to pipe audio to a socket"
-    sed -i 's|^; default-server.*$|default-server = unix:/tmp/pulseaudio.socket|' /etc/pulse/client.conf
-    sed -i 's|^load-module module-native-protocol-unix.*$|load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket auth-anonymous=1|' \
+    mkdir -p /tmp/pulse
+    chmod -R a+rw /tmp/pulse
+    sed -i 's|^; default-server.*$|default-server = unix:/tmp/pulse/pulse-socket|' /etc/pulse/client.conf
+    sed -i 's|^load-module module-native-protocol-unix.*$|load-module module-native-protocol-unix socket=/tmp/pulse/pulse-socket auth-anonymous=1|' \
         /etc/pulse/default.pa
 fi
 chown -R ${USER} /etc/pulse
