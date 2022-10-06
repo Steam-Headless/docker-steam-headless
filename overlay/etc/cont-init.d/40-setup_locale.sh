50cff47af7deb6ed402975c6e8c4fb886e950167
Xuereb <jonxuereb@gmail.com>
2022-10-06 10:25:27 -0400

    Revert "Merge branch 'mergable'"
    This reverts commit cc627b45706992e9b0d8a5f1c6d6bb8c3ee9d06e, reversing
    changes made to 6c21ca9af829a338982c95102cc3b7c46825b974.

--- overlay/etc/cont-init.d/40-setup_locale.sh
+++ overlay/etc/cont-init.d/40-setup_locale.sh
@@ -3,7 +3,7 @@ current_local=$(head -n 1 /etc/locale.gen)
 user_local=$(echo ${USER_LOCALES} | cut -d ' ' -f 1)
 
 if [ "${current_local}" != "${USER_LOCALES}" ]; then
-    echo "**** Configuring Locales from ${current_local} to ${USER_LOCALES} ****";
+    echo "**** Configuring Locales to ${USER_LOCALES} ****";
 	rm /etc/locale.gen
 	echo -e "${USER_LOCALES}\nen_US.UTF-8 UTF-8" > "/etc/locale.gen"
 	export LANGUAGE="${user_local}"
@@ -11,7 +11,7 @@ if [ "${current_local}" != "${USER_LOCALES}" ]; then
 	export LC_ALL="${user_local}" 2> /dev/null
 	sleep 2
 	locale-gen
-	# update-locale LC_ALL="${user_local}"
+	update-locale LC_ALL="${user_local}"
 else
     echo "**** Locales already set correctly to ${USER_LOCALES} ****";
 fi
--- overlay/etc/cont-init.d/60-configure_gpu_driver.sh
+++ overlay/etc/cont-init.d/60-configure_gpu_driver.sh
@@ -41,17 +41,26 @@ function download_driver {
 function install_nvidia_driver {
     # Check here if the currently installed version matches using nvidia-settings
     nvidia_settings_version=$(nvidia-settings --version 2> /dev/null | grep version | cut -d ' ' -f 4)
-    [[ "${nvidia_settings_version}x" == "${nvidia_host_driver_version}x" ]] && echo "Nvidia driver version matches the host" && return 0;
+    [[ "${nvidia_settings_version}x" == "${nvidia_host_driver_version}x" ]] && return 0;
 
     # Download the driver (if it does not yet exist locally)
     download_driver
 
+    # if command -v pacman &> /dev/null; then
+    #     echo "Install NVIDIA vulkan utils" \
+    #         && pacman -Syu --noconfirm --needed \
+    #             lib32-nvidia-utils \
+    #             lib32-vulkan-icd-loader
+    #             nvidia-utils \
+    #             vulkan-icd-loader \
+    #         && echo
+    # fi
+
     echo "Installing NVIDIA driver v${nvidia_host_driver_version} to match what is running on the host"
     chmod +x ${USER_HOME}/Downloads/NVIDIA_${nvidia_host_driver_version}.run
-    sudo ${USER_HOME}/Downloads/NVIDIA_${nvidia_host_driver_version}.run \
+    ${USER_HOME}/Downloads/NVIDIA_${nvidia_host_driver_version}.run \
         --silent \
         --accept-license \
-        --ui=none \
         --no-kernel-module \
         --install-compat32-libs \
         --no-nouveau-check \
@@ -62,9 +71,6 @@ function install_nvidia_driver {
         --no-libglx-indirect \
         --no-install-libglvnd \
         > ${USER_HOME}/Downloads/nvidia_gpu_install.log 2>&1
-
-    # Cleanup  
-    # rm -rf ${USER_HOME}/Downloads/NVIDIA_${nvidia_host_driver_version}.run
 }
 
 function install_amd_gpu_driver {
@@ -114,14 +120,13 @@ if [[ ! -z ${nvidia_pci_address} ]]; then
 else
     echo "**** No NVIDIA device found ****";
 fi
-# Intel GPU 
-## WTF IS THIS DOING GPU? CPU?
-# if [[ ! -z ${intel_cpu_model} ]]; then
-#     echo "**** Found Intel device '${intel_cpu_model}' ****";
-#     install_intel_gpu_driver
-# else
-#     echo "**** No Intel device found ****";
-# fi
+# Intel GPU
+if [[ ! -z ${intel_cpu_model} ]]; then
+    echo "**** Found Intel device '${intel_cpu_model}' ****";
+    install_intel_gpu_driver
+else
+    echo "**** No Intel device found ****";
+fi
 # AMD GPU
 if [[ ! -z ${amd_gpu_model} ]]; then
     echo "**** Found AMD device '${amd_gpu_model}' ****";
