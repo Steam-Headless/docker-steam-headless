54e4e09d294fd0e6c20e7246c9c7f1dd7998ed87
Josh.5 <jsunnex@gmail.com>
2022-10-09 12:30:57 +1300

    Fix error where on a contrainer restart you could not add default user to docker gourp again

--- overlay/etc/cont-init.d/80-configure-dind.sh
+++ overlay/etc/cont-init.d/80-configure-dind.sh
@@ -10,12 +10,14 @@ if ([ "${MODE}" != "s" ] && [ "${MODE}" != "secondary" ]); then
         echo "Docker socket has been passed in from host. Using that instead"
     fi
     # Configure 'default' user to run docker commands without sudo
-    echo "Add user '${USER}' to docker group for sudoless execution"
-    groupadd docker
-    usermod -aG docker ${USER}
-    mkdir -p /home/${USER}/.docker
-    chown -R ${PUID}:${PGID} /home/${USER}/.docker
-    chmod -R g+rwx /home/${USER}/.docker
+    if ! getent group docker &> /dev/null; then
+        echo "Add user '${USER}' to docker group for sudoless execution"
+        groupadd docker
+        usermod -aG docker ${USER}
+        mkdir -p /home/${USER}/.docker
+        chown -R ${PUID}:${PGID} /home/${USER}/.docker
+        chmod -R g+rwx /home/${USER}/.docker
+    fi
 else
     echo "Dockerd daemon not available when container is run in 'secondary' mode"
 fi
