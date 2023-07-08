cfad62f1cf1adadade54fde23e5440ad9a8a3d36
Josh.5 <jsunnex@gmail.com>
2023-07-08 15:58:40 +1200

    Display the progress of desktop app installation before starting the desktop
    This just prevents people from seeing a black screen for a long time and assuming something is going wrong.
    This initial setup is installaing flatpaks to the mounted home directory. This will not need to happen on every container startup. So this black screen is only a problem on the first start. But this change at least gives people something to look at.
    
    Closes #50

--- overlay/opt/scripts/install_protonup.sh
+++ overlay/opt/scripts/install_protonup.sh
@@ -4,7 +4,7 @@ echo "**** Installing/upgrading ProtonUp-Qt via flatpak ****"
 
 # Install ProtonUp-Qt
 flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
-flatpak --user install --assumeyes --noninteractive --or-update net.davidotek.pupgui2
+flatpak --user install --assumeyes --or-update net.davidotek.pupgui2
 
 # Configure Firefox as the default browser
 echo "Configure ProtonUp-Qt..."
