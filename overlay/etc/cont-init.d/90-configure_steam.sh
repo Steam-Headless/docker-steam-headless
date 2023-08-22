02a94566b2ed9234b6f09f135f502e7b39aa28ba
Josh.5 <jsunnex@gmail.com>
2023-08-22 15:02:35 +1200

    Revert to starting steam from desktop autostart

--- overlay/etc/cont-init.d/90-configure_steam.sh
+++ overlay/etc/cont-init.d/90-configure_steam.sh
@@ -1,15 +1,31 @@
 
 echo "**** Configure Steam ****"
 
+steam_autostart_desktop="$(cat <<EOF
+[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+Name=Steam
+Comment=Launch steam on login
+Exec=/usr/games/steam %U ${STEAM_ARGS:-}
+Icon=steam
+OnlyShowIn=XFCE;
+RunHook=0
+StartupNotify=false
+Terminal=false
+Hidden=false
+EOF
+)"
+
 if [ "${ENABLE_STEAM:-}" = "true" ]; then
-    # echo "Enable Steam service"
-    # sed -i 's|^autostart.*=.*$|autostart=true|' /etc/supervisor.d/steam.ini
-    # Remove old autostart script. We should be starting steam with the supervisor service
-    mkdir -p "${USER_HOME:?}/.config/autostart"
-    # if grep "Exec=/usr/bin/flatpak" "${USER_HOME:?}/.config/autostart/Steam.desktop" &> /dev/null; then
-    #     rm -fv "${USER_HOME:?}/.config/autostart/Steam.desktop"
-    # fi
-    rm -fv "${USER_HOME:?}/.config/autostart/Steam.desktop"
+    if [ "${MODE}" == "s" ] | [ "${MODE}" == "secondary" ]; then
+        echo "Enable Steam supervisor.d service"
+        sed -i 's|^autostart.*=.*$|autostart=true|' /etc/supervisor.d/steam.ini
+    else
+        echo "Enable Steam auto-start script"
+        mkdir -p "${USER_HOME:?}/.config/autostart"
+        echo "${steam_autostart_desktop:?}" > "${USER_HOME:?}/.config/autostart/Steam.desktop"
+    fi
 else
     echo "Disable Steam service"
     sed -i 's|^autostart.*=.*$|autostart=false|' /etc/supervisor.d/steam.ini
