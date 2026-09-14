975934118dd65da14b6aa12ea74dede5d0f6eeef
Josh.5 <jsunnex@gmail.com>
2026-09-15 09:19:53 +1200

    Update minimum `vm.max_map_count` to 1048576

--- overlay/etc/cont-init.d/11-setup_sysctl_values.sh
+++ overlay/etc/cont-init.d/11-setup_sysctl_values.sh
@@ -1,17 +1,15 @@
-
 # Configure kernel parameters
 print_header "Configure some system kernel parameters"
 
-
-if [ "$(cat /proc/sys/vm/max_map_count)" -le 524288 ]; then
+if [ "$(cat /proc/sys/vm/max_map_count)" -lt 1048576 ]; then
     if [ -w "/proc/sys/vm/max_map_count" ]; then
-        print_step_header "Setting the maximum number of memory map areas a process can create to 524288"
-        echo 524288 > /proc/sys/vm/max_map_count
+        print_step_header "Setting vm.max_map_count to the minimum required value of 1048576"
+        echo 1048576 >/proc/sys/vm/max_map_count
     else
-        print_warning "Unable to set vm.max_map_count on unprivileged container"
+        print_warning "vm.max_map_count is below the required minimum of 1048576. This unprivileged container cannot change host sysctls; configure vm.max_map_count=1048576 on the host."
     fi
 else
-    print_step_header "The vm.max_map_count is already greater than '524288'"
+    print_step_header "vm.max_map_count is already at or above 1048576; leaving the host value unchanged"
 fi
 
 echo -e "\e[34mDONE\e[0m"
